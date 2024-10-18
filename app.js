88a22ecc34a621cc879b0b4c9f7ad8c56f58ba1b
DEVANSHU PATIL <152208944+patildevanshu@users.noreply.github.com>
2024-10-18 23:46:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const http = require("http").Server(app);
+
+// mongo db
+
 const mongoose = require("mongoose");
 
 const userRouter = require('./routes/userRoute');
